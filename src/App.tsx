3a5d709519341972a25d6d97f1a43f1aa4eda072
Bobsbabysteps <paulreaume@protonmail.com>
2025-09-28 18:47:37 -0700

    housekeeping(home/app): keep short Home, remove legacy blocks and beta routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import EphoriaXHome from "./pages/EphoriaXHome";
 import PDF from "./pages/PDF";
-import BetaSignup from "./pages/BetaSignup";
-import PDFSubmit from "./pages/PDFSubmit";
 
 export default function App() {
   return (
@@ -10,8 +8,6 @@ export default function App() {
       <Routes>
         <Route path="/" element={<EphoriaXHome />} />
         <Route path="/pdf" element={<PDF />} />
-        <Route path="/beta" element={<BetaSignup />} />
-        <Route path="/pdf/submit" element={<PDFSubmit />} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
