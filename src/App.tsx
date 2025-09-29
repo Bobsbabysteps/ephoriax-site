bf4118d7d0772df2521193ee22a7a465a1bf1ca6
Bobsbabysteps <paulreaume@protonmail.com>
2025-09-29 07:24:07 -0700

    build(tailwind): switch from CDN to PostCSS pipeline

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+// src/App.tsx
+import { Routes, Route, Navigate } from "react-router-dom";
 import EphoriaXHome from "./pages/EphoriaXHome";
 import PDF from "./pages/PDF";
 
 export default function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<EphoriaXHome />} />
-        <Route path="/pdf" element={<PDF />} />
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<EphoriaXHome />} />
+      <Route path="/pdf" element={<PDF />} />
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
   );
 }
