e41456b070ef78d0cd01a6efeb716a92c0b30621
Bobsbabysteps <paulreaume@protonmail.com>
2025-11-02 10:14:56 -0800

    Fix dynamic Vite base path for production (resolve white screen)

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./", // ✅ ensure assets resolve correctly in Vercel
+  base: process.env.NODE_ENV === 'production' ? './' : '/',
+  build: {
+    outDir: 'dist',
+  },
 });
